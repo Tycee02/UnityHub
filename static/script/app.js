cff28ccd7a49f67550232ba83d7f23ba2f6c2921
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-05-30 22:14:25 +0100

    Update app.js

---
+++ static/script/app.js
@@ -0,0 +1,13 @@
+function showSection(sectionId, event) {
+    event.preventDefault();
+}
+
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+      section.classList.remove('active');
+    });
+
+    const activeSection = document.getElementById(sectionId);
+    activeSection.classList.add('active');
+}
